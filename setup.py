f016d0ee6e79e7f36c4d9ae0e237a1e3858a0c55
DS <61273115+136645224@users.noreply.github.com>
2024-08-12 17:33:47 +0800

    Update setup.py
    Delete some comments

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='wlkatapython', # 自定义包名
-    version='0.0.9', # 包的版本号
-    description='WLKATA-Mirobot/E4 Multiple robotic arm control!', # 描述信息
+    name='wlkatapython', 
+    version='0.0.9', 
+    description='WLKATA-Mirobot/E4 Multiple robotic arm control!', 
     long_description='Suitable for controlling single or multiple Mirobot robotic arms, E4 robotic arms, slides, and conveyor belts. Note that a multifunctional controller must be used to use this SDK.',
-    author='DS', # 作者
+    author='DS', 
     py_modules=[
         'wlkatapython'
-    ] ,# 包中包含的模块
+    ] ,
     install_requires=[
         'pyserial'
     ]
